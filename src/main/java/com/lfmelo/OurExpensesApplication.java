c007c6e1335a4507c742c2987b85a0e00276683f
femelo22 <luiz123jfmg@gmail.com>
2022-07-08 17:18:57 -0300

    start expensives project

---
+++ src/main/java/com/lfmelo/OurExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.lfmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurExpensesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lfmelo/entities/Address.java
@@ -0,0 +1,87 @@
+package com.lfmelo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "TB_ADDRESS")
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String street;
+	
+	private String neighborhood;
+	
+	private String complement;
+	
+	private String uf;
+	
+	private String cep;
+	
+	private String locality;
+	
+	public Address() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLocality() {
+		return locality;
+	}
+
+	public void setLocality(String locality) {
+		this.locality = locality;
+	}
+}
